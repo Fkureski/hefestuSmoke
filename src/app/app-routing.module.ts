8b205c6b2ca513274f32c1918e3f5ed16f617868
Your Name <felipepoulos@gmail.com>
2024-04-01 19:21:18 -0300

    Routers arrumados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/homepage',
+    pathMatch: 'full'
+  },
+  {
     path: 'homepage',
     component: HomepageComponent,
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent
   }
 ];
 
